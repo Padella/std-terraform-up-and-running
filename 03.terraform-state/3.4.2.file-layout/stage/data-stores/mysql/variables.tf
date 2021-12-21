292a1fdabc6998499a2ed6cfb6d739b904fea72c
Padella <execve@kakao.com>
2021-12-22 01:05:15 +0900

    feat: mysql RDS 생성 코드 추가

---
+++ 03.terraform-state/3.4.2.file-layout/stage/data-stores/mysql/variables.tf
@@ -0,0 +1,7 @@
+variable "db_password" {
+  description = "The password for the database"
+  type        = string
+  # 원래는 password 같은 내용은 default 옵션을 통해 평문으로 저장하면 안 되지만...
+  # 공부하는 환경에서는 귀찮으니 일단..
+  default     = "admin"
+}
