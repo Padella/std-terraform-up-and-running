ea6c7a69d9738a6dd4ff53afe1cd544a5c1ae1e2
Padella <execve@kakao.com>
2021-12-22 01:12:07 +0900

    fix: aws db admin password 수정 +  admin password 는 8글자 이상 따라서 default 값 수정

--- 03.terraform-state/3.4.2.file-layout/stage/data-stores/mysql/variables.tf
+++ 03.terraform-state/3.4.2.file-layout/stage/data-stores/mysql/variables.tf
@@ -3,5 +3,5 @@ variable "db_password" {
   type        = string
   # 원래는 password 같은 내용은 default 옵션을 통해 평문으로 저장하면 안 되지만...
   # 공부하는 환경에서는 귀찮으니 일단..
-  default     = "admin"
+  default     = "adminadmin"
 }
