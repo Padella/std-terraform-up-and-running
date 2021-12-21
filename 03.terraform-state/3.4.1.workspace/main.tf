554eb690b8611db6776b91daa3d5692fda8db70b
Padella <execve@kakao.com>
2021-12-22 00:08:44 +0900

    feat: 3.4.1 workspace 분리 + workspace 분리에 따른 tf 동작 학습 코드 추가

--- 03.terraform-state/3.4.1.workspace/main.tf
+++ 03.terraform-state/3.4.1.workspace/main.tf
@@ -5,16 +5,17 @@ provider "aws" {
 terraform {
   backend "s3" {
     // 3.2 코드에서 생성한 s3 bucket 및 dynamoDB table 을 사용한다.
-    bucket = "example-tf-state"
+    bucket = "padella-tf-state-example"
     key    = "workspaces-example/terraform.tfstate"
     region = "us-east-2"
 
-    dynamodb_table = "example-tf-locks"
+    dynamodb_table = "padella-tf-locks-example"
     encrypt        = true
   }
 }
 
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+  // instance_type = "t2.micro"
+  instance_type = terraform.workspaces == "default" ? "t2.medium" : "t2.micro"
 }
