52dd2b2dc8143174ba6a0af29eb0fd6dddaa3980
Padella <execve@kakao.com>
2021-12-25 03:04:36 +0900

    feat: module 에 custom tag 생성 코드 추가 + for_each 문을 통해 custom tag를 생성하는 코드 추가

--- 05.tips-and-tricks/5.1.loops-and-if-statements/ modules/modules/services/webserver-cluster/variables.tf
+++ 05.tips-and-tricks/5.1.loops-and-if-statements/ modules/modules/services/webserver-cluster/variables.tf
@@ -43,3 +43,9 @@ variable "server_port" {
   type        = number
   default     = 8080
 }
+
+variable "custom_tags" {
+  description = "Custom tags to set on the Instances in the ASG"
+  type        = map(string)
+  default     = {}
+}
