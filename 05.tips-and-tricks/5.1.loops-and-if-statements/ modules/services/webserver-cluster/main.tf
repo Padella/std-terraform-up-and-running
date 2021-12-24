49c261e7af004dc9982275eb3894d546ceb558b8
Padella <execve@kakao.com>
2021-12-25 03:07:50 +0900

    fix: 모듈 경로 오류 수정 + module/module/.. 로 되어있던 경로 오류 수정 + s3 backend 주석 처리   + local 테스트 환경에서 상용하지 않음

--- 05.tips-and-tricks/5.1.loops-and-if-statements/ modules/services/webserver-cluster/main.tf
+++ 05.tips-and-tricks/5.1.loops-and-if-statements/ modules/services/webserver-cluster/main.tf
@@ -152,17 +152,17 @@ resource "aws_security_group" "alb" {
   }
 }
 
-data "terraform_remote_state" "db" {
-  backend = "s3"
+// data "terraform_remote_state" "db" {
+//   backend = "s3"
 
-  config = {
-    # bucket = var.db_remote_state_bucket
-    # key    = var.db_remote_state_key
-    bucket = "padella-tf-state-example"
-    key    = "stage/data-stores/mysql/terraform.tfstate"
-    region = "us-east-2"
-  }
-}
+//   config = {
+//     # bucket = var.db_remote_state_bucket
+//     # key    = var.db_remote_state_key
+//     bucket = "padella-tf-state-example"
+//     key    = "stage/data-stores/mysql/terraform.tfstate"
+//     region = "us-east-2"
+//   }
+// }
 
 # local 값 지정
 locals {
